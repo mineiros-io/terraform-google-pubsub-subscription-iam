ed36c90b05859d3142637b2edd2e717d09a66cc6
Hany Elfouly <Hany@mineiros.io>
2021-10-18 13:13:06 +0200

    feat: add google pubsub subscription iam policy

--- variables.tf
+++ variables.tf
@@ -8,15 +8,16 @@ variable "subscription" {
   type        = string
 }
 
-variable "role" {
-  description = "(Required) The role that should be applied. Note that custom roles must be of the format '[projects|organizations]/{parent-name}/roles/{role-name}'."
-  type        = string
-}
-
 # ---------------------------------------------------------------------------------------------------------------------
 # OPTIONAL VARIABLES
 # These variables have defaults, but may be overridden.
 # ---------------------------------------------------------------------------------------------------------------------
+
+variable "role" {
+  description = "(Optional) The role that should be applied. Note that custom roles must be of the format '[projects|organizations]/{parent-name}/roles/{role-name}'."
+  type        = string
+  default     = null
+}
 
 variable "project" {
   description = "(Optional) The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
@@ -28,6 +29,12 @@ variable "members" {
   type        = set(string)
   description = "(Optional) Identities that will be granted the privilege in role. Each entry can have one of the following values: 'allUsers', 'allAuthenticatedUsers', 'serviceAccount:{emailid}', 'group:{emailid}', 'domain:{domain}'."
   default     = []
+}
+
+variable "policy_bindings" {
+  description = "(Optional) A list of IAM policy bindings."
+  type        = any
+  default     = null
 }
 
 variable "authoritative" {
